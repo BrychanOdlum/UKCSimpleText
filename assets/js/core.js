4cc595ead36641e944aae9fc8857f6795ce9c5ea
School <school@MacBook.local>
2017-02-27 13:30:26 +0000

    No scoll hackin'

--- assets/js/core.js
+++ assets/js/core.js
@@ -5,10 +5,13 @@ window.onload = function(){
 	taElem.addEventListener('click', keyPressed);
 	taElem.addEventListener('keydown', keyPressed);
 	taElem.addEventListener('keyup', keyPressed);
+	window.onresize = resize;
 	document.onkeydown = dKeyPressed;
 	
 	drawLineHighlight()
 	generateLineNumbers()
+	
+	setInterval(keyPressed, 20);
 }
 
 function scrolling(event) {
@@ -18,14 +21,14 @@ function scrolling(event) {
 
 function keyPressed(event) {
 	drawLineHighlight()
+	setTASize()
 	generateLineNumbers()
 }
 
-
 function dKeyPressed(event) {
 	var taElem = document.getElementById('ta');
-	if(event.keyCode == 9) {
-		if (document.activeElement === taElem) {
+	if (document.activeElement === taElem) {
+		if(event.keyCode == 9) {
 			var start = taElem.selectionStart
 			var val = taElem.value
 			taElem.value = val.substring(0, start) + "	" + val.substring(taElem.selectionEnd)
@@ -33,7 +36,19 @@ function dKeyPressed(event) {
 			taElem.focus()
 			return false;
 		}
+		if(event.keyCode == 13) {
+			console.log("added")
+			var tapElem = document.getElementById('ta-p');
+			tapElem.innerHTML += "<br>"
+			drawLineHighlight()
+			setTASize()
+			generateLineNumbers()
+		}
 	}
+}
+
+function resize() {
+	setTASize()
 }
 
 
@@ -53,7 +68,7 @@ function drawLineHighlight() {
 	lineHeight = parseInt(lineHeight.replace('px', ''));
 	var scrollTop = taElem.scrollTop
 	
-	document.getElementById('lh').style.top = ((line*lineHeight) + (2 - scrollTop)) + 'px';
+	document.getElementById('lh').style.top = ((line*lineHeight) + (scrollTop)) + 'px';
 	
 }
 
@@ -63,22 +78,37 @@ function getCurrentLine() {
 }
 
 function getHeightBeforeLine(line) {
+	var taElem = document.getElementById('ta');
+	var tapElem = document.getElementById('ta-p');
 	
+	tapElem.innerHTML = taElem.value
+	tapElem.innerHTML = tapElem.innerHTML.replace(/</g, '&lt;');
+	tapElem.innerHTML = tapElem.innerHTML.replace(/>/g, '&gt;');
+	tapElem.innerHTML = tapElem.innerHTML.replace(/\n/g, '<br>');
+	tapElem.innerHTML = tapElem.innerHTML.replace(/ /g, '&nbsp;');
+	tapElem.innerHTML = tapElem.innerHTML.replace(/\t/g, '&#09;');
+	tapElem.innerHTML += "<br>"
+}
+
+
+function setTASize() {
+	var taElem = document.getElementById('ta');
+	var tapElem = document.getElementById('ta-p');
+	
+	taElem.style.height = tapElem.offsetHeight + 'px'
 }
 
 function generateLineNumbers() {
 	var lnElem = document.getElementById('ln');
 	var taElem = document.getElementById('ta');
 	var lines = taElem.value.split("\n").length
-	var scrollTop = taElem.scrollTop
 	
 	var linesText = ""
 	for (var i = 1; i <= lines; i++) {
 		linesText += i + "<br>"
 	}
 	
-	lnElem.innerHTML = linesText;
+	getHeightBeforeLine(lines)
 	
-	console.log(scrollTop)
-	lnElem.scrollTop = scrollTop
+	lnElem.innerHTML = linesText;
 }
